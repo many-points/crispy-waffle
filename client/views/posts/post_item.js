9d532ea39f7f725537116a9596a58d7eb37a0409
deloff <patch419@gmail.com>
2015-10-22 23:42:08 +0300

    m

--- client/views/posts/post_item.js
+++ client/views/posts/post_item.js
@@ -6,5 +6,8 @@ Template.postItem.helpers({
   },
   ownPost: function () {
     return this.userId === Meteor.userId();
+  },
+  commentsCount: function () {
+    return this.commentsCount;
   }
 });
