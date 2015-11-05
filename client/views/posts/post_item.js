8d5a29026d6fa46cd9d180ea57fd2ba8d08a7304
deloff <patch419@gmail.com>
2015-11-05 22:45:00 +0300

    u

--- client/views/posts/post_item.js
+++ client/views/posts/post_item.js
@@ -1,3 +1,6 @@
+var POST_HEIGHT = 80;
+var Positions = new Meteor.Collection(null);
+
 Template.postItem.helpers({
   domain: function () {
     var a = document.createElement('a');
@@ -17,6 +20,23 @@ Template.postItem.helpers({
     } else {
       return 'btn-default disabled';
     }
+  },
+  attributes: function() {
+    var post = _.extend({}, Positions.findOne({postId: this._id}), this);
+    var newPosition = post._rank * POST_HEIGHT;
+    var attributes = {};
+    if (_.isUndefined(post.position)) {
+      attributes.class = 'post invisible';
+    } else {
+      var offset = post.position - newPosition;
+      attributes.style = "top: " + offset + "px";
+      if (offset === 0)
+        attributes.class = "post animate";
+    }
+    Meteor.setTimeout(function() {
+      Positions.upsert({postId: post._id}, {$set: {position: newPosition}});
+    });
+    return attributes;
   }
 });
 
